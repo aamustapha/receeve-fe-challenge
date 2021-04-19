f89493baf599b77dbf6051e0ffa17aa081fbae85
aamustapha <abdulhakeemmustapha@gmail.com>
2021-04-19 20:04:50 +0100

    retrieve some summaries from datasets

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,53 @@
 <template>
   <div>
-    dashboard
+    {{  claimsByDueMonth  }}
   </div>
 </template>
 <script lang="ts">
-import {Component, Vue, Prop} from "vue-property-decorator";
+import {Component, Vue } from "vue-property-decorator";
+import {Claim} from "@/models";
+import axios from "axios";
 
 @Component
 export default class Dashboard extends Vue {
-  @Prop()
-  msg!: string;
+  claims: Array<Claim> = []
+
+  get claimsByCompletion () {
+    return this.claims.reduce((accumulator, current) => {
+      if (!accumulator[current.status]) {
+        accumulator[current.status] = 0
+      }
+      accumulator[current.status] += current.baseAmount
+      return accumulator
+    }, {})
+  }
+
+  get claimsByDueDate() {
+    return this.claims.reduce((accumulator, current) => {
+      if (!accumulator[current.dueDate]) {
+        accumulator[current.dueDate] = 0
+      }
+      accumulator[current.dueDate] += current.baseAmount
+      return accumulator
+    }, {})
+  }
+
+  get claimsByDueMonth() {
+    return this.claims.reduce((accumulator, current) => {
+      const dueMonth = current.dueDate.substr(0,7)
+      if (!accumulator[dueMonth]) {
+        accumulator[dueMonth] = 0
+      }
+      accumulator[dueMonth] += current.baseAmount
+      return accumulator
+    }, {})
+  }
+
+  created () {
+    axios.get(`http://localhost:9001/claims?_sort=dueDate`).then(response => {
+      this.claims = response.data as Array<Claim>
+    })
+  }
 }
 </script>
 
